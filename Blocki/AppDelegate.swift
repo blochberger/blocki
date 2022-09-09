d3894913c895cb0ee556ceea49d4796bd397363b
Maximilian Blochberger <blochberger.max@gmail.com>
2022-09-09 19:19:58 +0200

    Add help menu entry for viewing source on GitHub

--- Blocki/AppDelegate.swift
+++ Blocki/AppDelegate.swift
@@ -7,6 +7,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		NSWorkspace.shared.open(URL(string: "https://developer.apple.com/documentation/safariservices/creating_a_content_blocker")!)
 	}
 
+	@IBAction func viewOnGithub(_ sender: NSMenuItem) {
+		NSWorkspace.shared.open(URL(string: "https://github.com/blochberger/blocki")!)
+	}
+	
 	// MARK: NSApplicationDelegate
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {

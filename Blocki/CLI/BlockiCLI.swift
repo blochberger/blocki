427aab563894f325ef1e700cbb2522117441fc05
Maximilian Blochberger <blochberger.max@gmail.com>
2025-06-13 13:50:07 +0200

    Improve command-line help output (fixes #7)

--- Blocki/CLI/BlockiCLI.swift
+++ Blocki/CLI/BlockiCLI.swift
@@ -8,12 +8,19 @@ extension FileHandle: TextOutputStream {
 }
 
 struct UI: AsyncParsableCommand {
-    // Ignore system arguments for UI applications, such as
-    // `-NSShowNonLocalizedStrings`, `-ApplePersistenceIgnoreState`, etc.
-    @Argument(parsing: .captureForPassthrough) var arguments: [String] = []
+    @Argument(
+        parsing: .captureForPassthrough,
+        help: "Pass through arguments to the Blocki app, such as -NSShowNonLocalizedStrings or -ApplePersistenceIgnoreState."
+    ) var arguments: [String] = []
+
+    static let configuration = CommandConfiguration(abstract: "Run the Blocki app.")
 }
 
 struct Status: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Check the status of the extension.",
+        discussion: "The command returns with exit code 0 if the extension is active and 1 otherwise.")
+
     func run() async throws {
         let isEnabled = try await Blocki.isEnabled()
         print(isEnabled ? "The extension is enabled." : "The extension is not enabled.")
@@ -22,6 +29,8 @@ struct Status: AsyncParsableCommand {
 }
 
 struct Reload: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(abstract: "Reload the rules in Safari.")
+
     func run() async throws {
         try await Blocki.reloadBlocklist()
     }
@@ -30,6 +39,7 @@ struct Reload: AsyncParsableCommand {
 struct BlockiCLI: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "blocki",
+        abstract: "A Safari Content Blocker extension where you are in control.",
         version: Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String,
         subcommands: [UI.self, Status.self, Reload.self],
         defaultSubcommand: UI.self)
